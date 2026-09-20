57211abb46a15ae32e6d25b832c35e9981852b88
Kenley <kenley.abaka@gmail.com>
2026-09-20 00:02:33 -0400

    Comitting all changes made thus far for the setting up of the repo.

--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -1,12 +1,22 @@
 #include <chrono>
 #include <memory>
-
 #include "costmap_node.hpp"
 
-CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {}
+using namespace std::chrono_literals;
 
-int main(int argc, char ** argv)
-{
+CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {
+  string_pub_ = this->create_publisher<std_msgs::msg::String>("/test_topic", 10);
+  timer_ = this->create_wall_timer(500ms, std::bind(&CostmapNode::publishMessage, this));
+}
+
+void CostmapNode::publishMessage() {
+  auto message = std_msgs::msg::String();
+  message.data = "Hello, ROS 2!";
+  RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+  string_pub_->publish(message);
+}
+
+int main(int argc, char ** argv) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<CostmapNode>());
   rclcpp::shutdown();
