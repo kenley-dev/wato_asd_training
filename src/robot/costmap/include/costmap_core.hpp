5aa5344125da03bfc7aa8dcf328711afed032aaa
Kenley <kenley.abaka@gmail.com>
2026-09-22 23:48:12 -0400

    Initializing costmap

--- src/robot/costmap/include/costmap_core.hpp
+++ src/robot/costmap/include/costmap_core.hpp
@@ -2,6 +2,8 @@
 #define COSTMAP_CORE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include <vector>
 
 namespace robot
 {
@@ -11,9 +13,19 @@ class CostmapCore {
     // Constructor, we pass in the node's RCLCPP logger to enable logging to terminal
     explicit CostmapCore(const rclcpp::Logger& logger);
 
+    void updateCostmap(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+
+    const std::vector<std::vector<int>>& getCostmap() const;
+
   private:
     rclcpp::Logger logger_;
 
+    // 2D costmap representation
+    std::vector<std::vector<int>> costmap_;
+
+    double resolution_ = 0.1; // metres per cell
+    int width_ = 100; // number of cells in x-direction
+    int height_ = 100; // number of cells in y-direction
 };
 
 }  
